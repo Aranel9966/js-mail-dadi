c5a439c568046487a4a9e7d53c768309e10c2fde
Aranel9966 <99angelo.dire@gmail.com>
2023-02-27 12:10:05 +0100

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+/*
+-array di email
+-chiedere l'email all'utente
+?se l'email dell'utente corrisponde all'email nell'array apri 
+:? altrimenti nega l'accesso
+-stampa un messaggio appropriato
+
+*/
