bb18c4e51c0a454988e2897a39e10db9f6dada92
Aranel9966 <99angelo.dire@gmail.com>
2023-02-27 12:30:08 +0100

    add array and a event add input email

--- js/script.js
+++ js/script.js
@@ -9,5 +9,18 @@
 ?se l'email dell'utente corrisponde all'email nell'array apri 
 :? altrimenti nega l'accesso
 -stampa un messaggio appropriato
-
 */
+
+
+// creo un array di email
+let emails = ['pino@ciao.it','gino@ciao.it','pippo@ciao.it','topolino@ciao.it','paperino@ciao.it'];
+
+// richiamo i rispettivi id in delle variabili
+let userEmailEl = document.getElementById('userEmail');
+let submitEl = document.getElementById('submit');
+
+// creo un evento per prendere il valore dentro l'input al premere del bottone
+submitEl.addEventListener('click', function(){
+    console.log(userEmailEl.value)
+
+})
