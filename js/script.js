55ee078b4e3b5d6fd3cd2bdffd2ec87381dcf0e8
Aranel9966 <99angelo.dire@gmail.com>
2023-02-27 14:06:49 +0100

    add if and for with print control

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 /*
 -array di email
 -chiedere l'email all'utente
+* crea un ciclo per controllare l'email
 ?se l'email dell'utente corrisponde all'email nell'array apri 
 :? altrimenti nega l'accesso
 -stampa un messaggio appropriato
@@ -18,9 +19,49 @@ let emails = ['pino@ciao.it','gino@ciao.it','pippo@ciao.it','topolino@ciao.it','
 // richiamo i rispettivi id in delle variabili
 let userEmailEl = document.getElementById('userEmail');
 let submitEl = document.getElementById('submit');
+let printEl = document.getElementById('print')
 
 // creo un evento per prendere il valore dentro l'input al premere del bottone
 submitEl.addEventListener('click', function(){
-    console.log(userEmailEl.value)
+    let access ='';
+    //ciclo di controllo email
+    for(i = 0 ; i < emails.length ; i++ ){
+
+        // console.log(emails[i])
+        // console.log(userEmailEl.value)
+
+
+        if(userEmailEl.value == emails[i]){
+            access = 1;
+            console.log('accesso riuscito')
+
+        }else{
+            console.log('accesso NON riuscito')
+
+        }
+
+    }
+    // controllo stampa
+
+    
+
+    if(access == 1 ){
+        printEl.innerText=('Accesso riuscito')
+        
+    }else{
+        printEl.innerText=('Accesso NON riuscito')
+
+    }
 
 })
+
+
+
+
+
+
+
+
+
+
+// esiste il metodo .includes() per trovare subito l'occorrenza di un elemento all'interno di un array
